2dd400b4bf00310c21d5c0650eea19a80ef4048c
Eric Kreis <erickreis793@gmail.com>
2022-08-10 02:10:24 -0300

    create: functions of list_length module

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: get_length(list, 0)
+
+  defp get_length([], acc), do: acc
+
+  defp get_length([_head | tail], acc) do
+    get_length(tail, acc + 1)
   end
 end
